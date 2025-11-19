303d475787fdc8078acc5f128ce6c96cdff6233b
mildebrandtj <mildebrandt@wisc.edu>
2025-11-18 18:34:11 -0600

    Update products.js

--- js/products.js
+++ js/products.js
@@ -33,8 +33,8 @@ function renderProducts(productArray) {
     });
 }
 //the actual search
-document.getElementById('search-bar').addEventListener('input', function(f) {
-    const query = f.target.value.toLowerCase();
+document.getElementById('search-bar').addEventListener('input', function(e) {
+    const query = e.target.value.toLowerCase();
     const filteredProducts = products.filter(product => product.name.toLowerCase().includes(query));
     renderProducts(filteredProducts);
   });
