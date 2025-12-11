d355c825a667fb3c8afa5f5eb2004eca0189a841
mildebrandtj <mildebrandt@wisc.edu>
2025-12-11 15:53:01 -0600

    switched out cards for html
    then added more content, made form function and copy to clipboard function

--- js/contact.js
+++ js/contact.js
@@ -1,42 +1,14 @@
-const container = document.getElementById("contact-card-row");
+const supportForm = document.getElementById('support-form');
 
-const cards = [
-  {
-    title: "Customer Support",
-    text: "Need help? Submit a support request form.",
-    buttonText: "Open Form",
-    buttonLink: "support-form.html" //NOTE: NEED TO ADD FUNCTIONALITY TO THIS LATER
-  },
-  {
-    title: "Instagram",
-    text: "Follow us for updates and news!",
-    buttonText: "Visit Instagram",
-    buttonLink: "https://instagram.com"
-  },
-  {
-    title: "Contact Info",
-    text: `
-      Phone: (555) 123-4567<br>
-      Email: support@smartgrocery.com
-    `,
-    buttonText: "Copy Info", //NOTE: NEED TO ADD FUNCTIONALITY TO THIS LATER
-    buttonLink: "#"
-  }
-];
+if (supportForm) {
+    supportForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      alert('Thank you for reaching out! We will get back to you shortly.');
+      supportForm.reset();
+    });
+}
 
-cards.forEach(card => {
-  const cardWrapper = document.createElement("div");
-  cardWrapper.className = "col-12 d-flex justify-content-center";
-
-  cardWrapper.innerHTML = `
-    <div class="card shadow-sm p-4 mb-4" style="max-width: 500px; width: 100%;">
-      <h4 class="mb-2">${card.title}</h4>
-      <p>${card.text}</p>
-      <a href="${card.buttonLink}" class="btn btn-primary">
-        ${card.buttonText}
-      </a>
-    </div>
-  `;
-
-  container.appendChild(cardWrapper);
-});
+function copyToClipboard(email) {
+  navigator.clipboard.writeText("Hello@SmartGrocery.com");
+  alert("Email address copied to clipboard: ");
+}
